65bd9e970f7b6e3d176968906a80cf142a4667c3
Olivia <51516158+oliviacarino@users.noreply.github.com>
2020-03-31 10:50:06 -0400

    Initial Commit

---
+++ midterm/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  midterm
+//
+//  Created by Olivia Carino on 3/31/20.
+//  Copyright © 2020 Olivia. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
